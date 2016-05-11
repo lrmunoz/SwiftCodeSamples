30045d513700573baeb5e7a102a68632a6b22cca
Luis Muñoz <luis@serendipio.com>
2016-05-11 11:50:34 +0200

    Add AFNetworking reachability test
    Add some simple sample test code of AFNEtworking reachability
    to illustrate this StackOverflow question:
    
    http://stackoverflow.com/questions/37155389/afnetworking-reachability-manager-in-swift/

---
+++ DataStructures/DataStructures/LinkedLists.swift
@@ -0,0 +1,9 @@
+//
+//  LinkedLists.swift
+//  DataStructures
+//
+//  Created by Luis Rafael Muñoz Hueso on 16/04/16.
+//  Copyright © 2016 Serendipio. All rights reserved.
+//
+
+import Foundation
---
+++ ReachabilityTest/ReachabilityTest/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ReachabilityTest
+//
+//  Created by Luis Rafael Muñoz Hueso on 11/05/16.
+//  Copyright © 2016 Serendipio. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view, typically from a nib.
+	}
+
+	override func didReceiveMemoryWarning() {
+		super.didReceiveMemoryWarning()
+		// Dispose of any resources that can be recreated.
+	}
+
+
+}
+
